a5ca00d43da636142a3008ccef401a5a69b5f891
zhzhr <zhzhr_9@sohu.com>
2020-07-17 09:43:57 +0800

    first commit

---
+++ VueTest/vue2/src/components/ExcelJsDemo.vue
@@ -0,0 +1,24 @@
+<template>
+    
+  <ExcelTable :data="data"></ExcelTable>
+</template>
+
+<script>
+import { ExcelTable } from '../../node_modules/exceljs/excel';
+
+export default {
+    data: function() {
+      return {
+        data: []
+      };
+    },
+    components: {
+      ExcelTable
+    }
+}
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ VueTest/vue2/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <hot-table :data="data" rowHeaders="true" colHeaders="true"></hot-table>
+</template>
+
+<script>
+import { HotTable } from '@handsontable/vue';
+
+  export default {
+    data: function() {
+      return {
+        data: [
+          ["", "Ford", "Volvo", "Toyota", "Honda"],
+          ["2016", 10, 11, 12, 13],
+          ["2017", 20, 11, 14, 13],
+          ["2018", 30, 15, 12, 13]
+        ],
+        licenseKey: 'ab3e4-1bee8-ed01c-4d94b-08cfe'
+      };
+    },
+    components: {
+      HotTable
+    }
+  }
+</script>
+
+<style src="../../node_modules/handsontable/dist/handsontable.full.css"></style>
